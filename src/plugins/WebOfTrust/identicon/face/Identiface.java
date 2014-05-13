0ebe28ee86eff9ca5bee13d38407e43c52ada1bd
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-05-13 21:29:45 +0200

    Use face definition to create head for rendering.

--- src/plugins/WebOfTrust/identicon/face/Identiface.java
+++ src/plugins/WebOfTrust/identicon/face/Identiface.java
@@ -5,18 +5,11 @@ import static java.awt.Color.white;
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
 import static java.lang.Math.PI;
-import static java.util.Arrays.asList;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.List;
-
-import plugins.WebOfTrust.identicon.BitShiftedInputStream;
 
 /**
  * Alternativ identicon implementation that draws faces from the bits of a
@@ -26,40 +19,29 @@ import plugins.WebOfTrust.identicon.BitShiftedInputStream;
  */
 public class Identiface {
 
-	private final byte[] data;
+	private final Face face;
 
-	public Identiface(byte[] data) {
-		this.data = data;
+	public Identiface(Face face) {
+		this.face = face;
 	}
 
 	public RenderedImage render(int width, int height) {
 		BufferedImage background = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D backgroundGraphcs = (Graphics2D) background.getGraphics();
+
 		backgroundGraphcs.setColor(white);
 		backgroundGraphcs.fillRect(0, 0, width, height);
-		BitShiftedInputStream bitStream = new BitShiftedInputStream(new ByteArrayInputStream(data), 8);
-		try {
-			Head head = Head.createHead(bitStream);
-			head.render(backgroundGraphcs, width, height);
-		} catch (IOException ioe1) {
-			/* should never happen. */
-			throw new RuntimeException(ioe1);
-		}
+
+		backgroundGraphcs.setColor(face.getBackgroundColor().getColor());
+		face.getBackgroundPattern().paint((Graphics2D) backgroundGraphcs.create(), width, height);
+
+		Head head = Head.createHead(face);
+		head.render(backgroundGraphcs, width, height);
+
 		return background;
 	}
 
 	private static class Head {
-
-		private static final List<Color> skinColors = asList(
-				new Color(0xff, 0xf8, 0xf0),
-				new Color(0xff, 0xf0, 0xe0),
-				new Color(0xff, 0xe8, 0xd0),
-				new Color(0xff, 0xe0, 0xc0),
-				new Color(0xff, 0xd0, 0xc0),
-				new Color(0xe0, 0xd0, 0x80),
-				new Color(0xd0, 0xb0, 0x80),
-				new Color(0xb0, 0x80, 0x50)
-		);
 
 		private final Oval head;
 		private final Oval leftEar;
@@ -82,16 +64,12 @@ public class Identiface {
 			rightEar.draw(graphics, width, height);
 		}
 
-		public static Head createHead(BitShiftedInputStream bitStream) throws IOException {
-			int headShape = bitStream.read(2);
-			int skinColor = bitStream.read(3);
-			int earWidth = bitStream.read(1);
-			int earHeight = bitStream.read(2);
+		public static Head createHead(Face face) {
 			return new Head(
-					0.5 + (0.3 * headShape / 4),
-					skinColors.get(skinColor),
-					0.12 + 0.04 * earWidth,
-					0.18 + 0.06 * earHeight / 4
+					face.getHeadShape().getWidth(),
+					face.getSkinColor().getColor(),
+					face.getEarWidth().getWidth(),
+					face.getEarHeight().getHeight()
 			);
 		}
 
